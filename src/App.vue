47aa50d42e81ce82a5e9c53216583ef419f0883c
Levente Madi | Corestad <levente.madi@corestad.com>
2022-05-02 19:00:29 +0200

    valami

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,61 @@
 <template>
-  <div class="container">
-    <div class="block"></div>
-    <button>Animate</button>
+<!-- <router-view></router-view> -->
+  <div>
+    <div class="container">
+      <users-list></users-list>
+    </div>
+    <div class="container">
+      <div class="block" :class="{ animate: animatedBlock }"></div>
+      <button @click="animateBlock">Animate</button>
+    </div>
+    <div class="container">
+      <transition
+        name="para"
+        :css="false"
+        @before-enter="beforeEnter"
+        @enter="enter"
+        @after-enter="afterEnter"
+        @before-leave="beforeLeave"
+        @leave="leave"
+        @after-leave="afterLeave"
+        @enter-cancelled="enterCancelled"
+        @leave-cancelled="leaveCancelled"
+      >
+        <p v-if="paraIsVisible">This is only sometimes visible</p>
+      </transition>
+      <button @click="togglePara">Toggle Paragraph</button>
+    </div>
+    <div class="container">
+      <transition name="fade-button" mode="in-out">
+        <button @click="showUsers" v-if="!usersAreVisible">Show Users</button>
+        <button @click="hideUsers" v-else>Hide Users</button>
+      </transition>
+    </div>
+    <base-modal @close="hideDialog" :open="dialogIsVisible">
+      <p>This is a test dialog!</p>
+      <button @click="hideDialog">Close it!</button>
+    </base-modal>
+    <div class="container">
+      <button @click="showDialog">Show Dialog</button>
+    </div>
   </div>
-  <base-modal @close="hideDialog" v-if="dialogIsVisible">
-    <p>This is a test dialog!</p>
-    <button @click="hideDialog">Close it!</button>
-  </base-modal>
-  <div class="container">
-    <button @click="showDialog">Show Dialog</button>
-  </div>
-</template>  
+</template>
 
 <script>
+import UsersList from './components/UsersList.vue';
 export default {
+  components: {
+    UsersList,
+  },
   data() {
-    return { dialogIsVisible: false };
+    return {
+      dialogIsVisible: false,
+      animatedBlock: false,
+      paraIsVisible: false,
+      usersAreVisible: false,
+      enterInterval: null,
+      leaveInterval: null,
+    };
   },
   methods: {
     showDialog() {
@@ -23,6 +63,63 @@ export default {
     },
     hideDialog() {
       this.dialogIsVisible = false;
+    },
+    animateBlock() {
+      this.animatedBlock = !this.animatedBlock;
+    },
+    togglePara() {
+      this.paraIsVisible = !this.paraIsVisible;
+    },
+    showUsers() {
+      this.usersAreVisible = true;
+    },
+    hideUsers() {
+      this.usersAreVisible = false;
+    },
+    beforeEnter(el) {
+      console.log('before enter');
+      el.style.opacity = 0;
+    },
+    enter(el, done) {
+      console.log('entered');
+      let round = 0;
+      this.enterInterval = setInterval(() => {
+        el.style.opacity = round * 0.01;
+        round++;
+        if (round > 100) {
+          clearInterval(this.enterInterval);
+          done();
+        }
+      }, 20);
+    },
+    afterEnter() {
+      console.log('after enter');
+    },
+    beforeLeave(el) {
+      console.log('before leave');
+      el.style.opacity = 1;
+    },
+    leave(el, done) {
+      console.log('leave');
+      console.log(el);
+      let round = 1;
+      this.leaveInterval = setInterval(() => {
+        el.style.opacity = 1 - round * 0.01;
+        round++;
+        if (round > 100) {
+          clearInterval(this.leaveInterval);
+          done();
+        }
+      }, 20);
+    },
+    afterLeave() {
+      console.log('after leave');
+    },
+    enterCancelled() {
+      clearInterval(this.enterInterval)
+    },
+    leaveCancelled() {
+      clearInterval(this.leaveInterval)
     },
   },
 };
@@ -57,6 +154,7 @@ button:active {
   height: 8rem;
   background-color: #290033;
   margin-bottom: 2rem;
+  /* transition: transform 0.3s ease-out; */
 }
 .container {
   max-width: 40rem;
@@ -68,5 +166,37 @@ button:active {
   padding: 2rem;
   border: 2px solid #ccc;
   border-radius: 12px;
+}
+
+.animate {
+  /* transform: translateX(-150px); */
+  animation: slide-scale 0.3s ease-out forwards;
+}
+
+.fade-button-enter-from {
+  opacity: 0;
+}
+.fade-button-enter-active {
+  transition: opacity 0.3s ease-out;
+}
+.fade-button-enter-to,
+.fade-button-leave-from {
+  opacity: 1;
+}
+
+.fade-button-leave-active {
+  transition: opacity 0.3s ease-in;
+}
+
+@keyframes slide-scale {
+  0% {
+    transform: translateX(0) scale(1);
+  }
+  70% {
+    transform: translateX(-120px) scale(1.1);
+  }
+  100% {
+    transform: translateX(-150px) scale(1);
+  }
 }
 </style>
